56f86e8bfcc2f29d7f992b0a79983ddd382020d9
Daisyyyyy0 <soniahsiao16@gmail.com>
2021-12-09 22:34:34 +0800

    test

---
+++ js/cart.js
@@ -0,0 +1,7 @@
+let addBtn = document.querySelectorAll('.added')
+let reduceBtn = document.querySelectorAll('.reduced')
+let itemInput = document.querySelectorAll('.itemInput')
+
+addBtn.forEach(function(btn){
+
+})
