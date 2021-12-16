8e42e41a6f91ac9a4193883a0bbee23b00537d61
DESKTOP-NQ7CSLI\daisy <soniahsiao16@gmail.com>
2021-12-16 17:10:17 +0800

    add scrollTop effect

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector('.navbar')
+// console.log(navbar.scrollTop);
+
+window.addEventListener('scroll',function(){
+    // console.log(document.body.);
+    console.log(window.scrollY);
+    if(window.scrollY > 0){
+        navbar.classList.add('navbar-bg-white');
+    }else if(window.scrollY === 0){
+        navbar.classList.remove('navbar-bg-white');
+    }
+})
