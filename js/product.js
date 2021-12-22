e561aefb3e1f4f29f4816e4fac0f80a7dbb402d5
DESKTOP-NQ7CSLI\daisy <soniahsiao16@gmail.com>
2021-12-22 23:53:45 +0800

    fix something

--- js/product.js
+++ js/product.js
@@ -1,14 +1,15 @@
+'use strict'
+
 // const gallery = document.querySelector('.gallery')
 const zoomImages = document.querySelectorAll('.zoomImage > img')
 const bigImage = document.querySelector('.bigImage > img')
 
-// zoomImages.forEach((i)=>{
-//     console.log(i);
-// })
-// console.log(bigImage);
+zoomImages.forEach((i)=>{
+    console.log(i);
+})
+console.log(bigImage);
 zoomImages.forEach(function(img,index){
     zoomImages[index].addEventListener('click', ()=>{
         bigImage.src = zoomImages[index].src
     })
-    
 })
